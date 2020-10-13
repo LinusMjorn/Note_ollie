cf1ff56b931e0ca379c16e9b92f04f1d927d498e
Victoria Jusko <vikjusko@gmail.com>
2020-10-13 16:05:12 +0100

    added test for empty list viewing

--- note-list-view-spec.js
+++ note-list-view-spec.js
@@ -4,4 +4,10 @@
 	 myList.create("I am creating a second note");
 	 var viewMyList = new NoteListView(myList); 
    assert.isEqualTo(viewMyList.viewList(),'<ul><li>I am creating a first note</li><li>I am creating a second note</li></ul>')
-  })();
+	})();
+	
+	(function(){
+	 var list = new NoteList();
+	 var viewList = new NoteListView(list); 
+	 assert.isEqualTo(viewList.viewList(),'<ul></ul>')
+	})();
