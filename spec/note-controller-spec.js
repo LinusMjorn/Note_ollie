e1bc15fd571a65b49c356766bf1ddfaabddc2342
farhaan-ali <fa8g12@gmail.com>
2020-10-14 16:07:46 +0100

    test 2 to check innerHTML is correct for Note Controller

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -1,7 +1,27 @@
+// Mocking
+class NoteListViewMock {
+  viewList() {
+    return "<ul><li><div>Favourite food: pesto</div></li></ul>";
+  }
+}
+
+// Test 1
 (function () {
   noteController = new NoteController();
   assertNew(
     noteController instanceof NoteController,
     "check that we can initiate a instance of NoteController"
+  );
+})();
+
+//Test 2
+(function () {
+  noteListViewMock = new NoteListViewMock();
+  noteController = new NoteController();
+  noteController.changeHTML("app", noteListViewMock.viewList());
+  var element = document.getElementById("app");
+  assertNew(
+    element.innerHTML == "<ul><li><div>Favourite food: pesto</div></li></ul>",
+    "check that we can change the innerHTML with Note Controller"
   );
 })();
